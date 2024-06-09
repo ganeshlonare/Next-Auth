8faf570eefa5680b200363d42d7c2ef4f02c8e2e
ganeshlonare <ganeshlonare311@gmail.com>
2024-06-09 22:54:21 +0530

    Added email verification API endpoint

---
+++ src/app/api/verifyemail/route.ts
@@ -0,0 +1,34 @@
+import { connect } from "@/db/dbConfig";
+import { NextRequest ,NextResponse } from "next/server";
+import User from "@/models/userModel";
+
+connect()
+
+export async function POST(req:NextRequest){
+    try {
+        const reqBody=await req.json()
+        const {token}=reqBody
+
+        const user=await User.findOne({verifyToken:token,verifyTokenExpiry:{$gt:Date.now()}})
+        if(!user){
+            return NextResponse.json({error:"Invalid token"},{status:400})
+        }
+        console.log(user)
+
+        user.isVerified=true
+        user.verifyToken=undefined
+        user.verifyTokenExpiry=undefined
+
+        await user.save()
+
+        return NextResponse.json({
+            message:"Email verified successfully",
+            success:true
+        },{status:200})
+
+        
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error:error},{status:500})
+    }
+}
