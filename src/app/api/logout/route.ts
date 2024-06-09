20b9e4bc2b5a8e556b06d4c305f9aca623b5b350
ganeshlonare <ganeshlonare311@gmail.com>
2024-06-09 23:19:52 +0530

    Implemented logout functionality.

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,23 @@
+import { connect } from "@/db/dbConfig";
+import { NextResponse , NextRequest } from "next/server";
+
+connect()
+
+export async function POSt(req:NextRequest){
+    try {
+        const response=NextResponse.json({
+            message:"Logged out Successfully",
+            success:true
+        },{status:200})
+
+        response.cookies.set("token","",{
+            httpOnly:true,
+            expires:new Date(0)
+        })
+
+        return response
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error:error},{status:500})
+    }
+}
